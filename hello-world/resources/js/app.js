bfb3c2a9f402e94570527985dd587d22159a1384
Grotawred <grishavlasko200@gmail.com>
2024-02-16 19:55:17 +0200

    first commit

---
+++ hello-world/resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+window.Vue = require('vue').default;
+
+Vue.component('hello-world', require('./components/HelloWorld.vue').default);
+
+
+const app = new Vue({
+    el: '#app',
+});
